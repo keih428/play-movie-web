c5aedd61edb07cdf5ef5e200f97415025543ca7f
keih428 <keiichiroharry884@gmail.com>
2026-05-26 06:46:16 -0300

    feature: completed

--- components/analysis-panel.tsx
+++ components/analysis-panel.tsx
@@ -376,8 +376,8 @@ export function AnalysisPanel({ match }: AnalysisPanelProps) {
               ) : (
                 <div className="timeline-stack">
                   {scoreTimeline.map((setTimeline) => {
-                    const width = 360;
-                    const height = 116;
+                    const width = 300;
+                    const height = 152;
                     const padding = { top: 10, right: 12, bottom: 16, left: 12 };
                     const chartWidth = width - padding.left - padding.right;
                     const chartHeight = height - padding.top - padding.bottom;
